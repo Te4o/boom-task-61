3d17c026e4913e1125f5a24ca37d204afcd0a110
Stoyan Dimitrov <stoyan.jordanov94@gmail.com>
2022-12-14 20:40:21 +0200

    task-61: hot price

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,10 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+  var priceElements = document.querySelectorAll('.price');
+  for(var i=0; i<=priceElements.length; i++){
+    if(priceElements[i].classList.contains("hot")){
+      priceElements[i].innerHTML+= '🔥';
+    }
+  }
 });
